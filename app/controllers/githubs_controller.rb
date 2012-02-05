159ff3f45693b230453fbe5b2e3039d6a7d62e76
Sean Grove <s@bushi.do>
2012-02-04 16:24:13 -0800

    Passes a human-readable message along with the GitHub hook event

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -1,12 +1,22 @@
 class GithubsController < ApplicationController
   def received
-    payload = params[:payload]
+    payload = params[:payload].symbolize_keys
 
     event = {}
     event[:category] = "git"
     event[:name] = :received
 
     event[:data] = payload
+
+    repo      = payload[:repository]
+    repo_name = repo[:name]
+    commits   = payload[:commits]
+    actor     = commits.last[:author][:name] || commits.last[:author][:email]
+    url       = commits.last[:url]
+    message   = commits.last[:message]
+    branch    = payload[:ref].split("/").last
+
+    event[:data][:human] = "#{actor} pushed to #{repo_name}/#{branch}, saying '#{message}' -> See more at #{url}"
 
     puts event.inspect
 
