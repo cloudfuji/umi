e44acf48380b3f541a9333e9b9b8ba70c19b5ee0
Sean Grove <s@bushi.do>
2012-02-29 16:04:33 -0800

    Updates Jenkins hook

--- app/controllers/jenkins_controller.rb
+++ app/controllers/jenkins_controller.rb
@@ -2,24 +2,33 @@ class JenkinsController < ApplicationController
   def notification
     puts params.inspect
 
-    event = {}
-    event[:category] = "build"
-    event[:data]     = params[:build] || {}
-    event[:data][:name] = params[:name]
+    # Because of the weird Jenkins header format...
+    data = JSON(params[:key], :symbolize_names => true)
 
-    
-    event[:name] = case params[:build][:phase].downcase
+    puts data.inspect
+
+    event               = {}
+    event[:category]    = "build"
+    event[:data]        = data[:build] || {}
+    event[:data][:name] = data[:name]
+
+
+    event[:name] = case data[:build][:phase].downcase
                    when "started"
                      "started"
                    when "finished"
-                     params[:build][:status]
+                     data[:build][:status]
                    end
 
     puts event.inspect
 
-    event[:data][:human] = "Build #{params[:name]} has #{event[:name]}"
+    event[:data][:human] = "Build #{data[:name]} ##{data[:build][:number]} has #{event[:name]}"
 
     Bushido::Event.publish(event)
+
+    User.all.collect do |user|
+      Bushido::User.notify(user.ido_id, "Build Update", event[:data][:human], "development")
+    end
 
     render :json => "OK"
   end
