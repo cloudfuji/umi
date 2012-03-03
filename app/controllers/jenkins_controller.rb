7c98860b572606b0828dcb367765fda71174e635
Sean Grove <s@bushi.do>
2012-03-03 12:28:01 -0800

    Attempts to handle Jenkins poor json formatting

--- app/controllers/jenkins_controller.rb
+++ app/controllers/jenkins_controller.rb
@@ -2,27 +2,40 @@ class JenkinsController < ApplicationController
   def notification
     puts params.inspect
 
-    # Because of the weird Jenkins header format...
-    data = JSON(params[:key], :symbolize_names => true)
+    data = nil
 
-    puts data.inspect
+    # Dealing with Jenkin's bad headers
+    # "{\"name\":\"bushido_master\",\"url\":\"job/bushido_master/\",\"build\":{\"number\":452,\"phase\":\"STARTED\",\"url\":\"job/bushido_master/452/\"}}"=>nil, "action"=>"notification", "controller"=>"jenkins", "format"=>"json"}
+    params.keys.each do |key|
+      begin
+        data = JSON(key)
+        break if data[:build]
+      rescue
+        puts "key: #{key} not deserializable from json"
+      end
+    end
+
+    if data.nil? || data[:build].nil?
+      puts "Returning blank due to badly formatted data:"
+      puts data.inspect
+    end
 
     event               = {}
     event[:category]    = "build"
-    event[:data]        = data[:build] || {}
-    event[:data][:name] = data[:name]
+    event[:data]        = data["build"] || {}
+    event[:data][:name] = data["name"]
 
 
-    event[:name] = case data[:build][:phase].downcase
+    event[:name] = case data["build"]["phase"].downcase
                    when "started"
                      "started"
                    when "finished"
-                     data[:build][:status]
+                     data["build"]["status"]
                    end
 
     puts event.inspect
 
-    event[:data][:human] = "Build #{data[:name]} ##{data[:build][:number]} has #{event[:name]}"
+    event[:data][:human] = "Build #{data['name']} ##{data['build']['number']} has #{event[:name]}"
 
     Bushido::Event.publish(event)
 
