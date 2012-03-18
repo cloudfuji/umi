370daf628583acde398973d46b9d78cf7e32cae1
Sean Grove <s@bushi.do>
2012-03-18 05:54:55 -0700

    Manually trigger the pivotal announcements since the observer isn't firing

--- lib/pivotal_tracker/pivotal_tracker_util.rb
+++ lib/pivotal_tracker/pivotal_tracker_util.rb
@@ -24,38 +24,45 @@ class PivotalTrackerUtil
     end
 
     def create_bushido_project(foreign_project)
-      project = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
+      project    = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
+      new_record = project.new_record?
       project.from_pivotal(foreign_project)
 
       puts project.inspect
       result = project.save
       puts "PROJECT SAVED? #{result}"
       puts project.errors.inspect
+      project.announce! if result && new_record?
       result
     end
 
     def create_bushido_story(foreign_project, foreign_story)
-      project = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
-      story = project.stories.find_or_initialize_by(:external_id => foreign_story.id)
+      project    = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
+      story      = project.stories.find_or_initialize_by(:external_id => foreign_story.id)
+      new_record = story.new_record?
       story.from_pivotal(foreign_story)
 
       puts story.inspect
       result = story.save
       puts "STORY SAVED? #{result}"
       puts story.errors.inspect
+      story.announce! if result && new_record?
       result
     end
 
     def create_bushido_note(foreign_project, foreign_story, foreign_note)
-      project = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
-      story   = project.stories.find_or_initialize_by(:external_id => foreign_story.id)
-      note    = story.notes.find_or_initialize_by(:external_id => foreign_note.id)
+      project    = Pivotal::Project.find_or_initialize_by(:external_id => foreign_project.id)
+      story      = project.stories.find_or_initialize_by(:external_id => foreign_story.id)
+      note       = story.notes.find_or_initialize_by(:external_id => foreign_note.id)
+      new_record = note.new_record?
       note.from_pivotal(foreign_note)
+      
 
       puts note.inspect
       result = note.save
       puts "NOTE SAVED? #{result}"
       puts note.errors.inspect
+      note.announce! if result && new_record?
       result
     end
   end
